0ba621258a7b3dcae80c74316c8d1b77c24782ef
JakobEvans <evansja@sonoma.edu>
2021-02-08 15:46:48 -0800

    Initial

---
+++ app/src/main/java/ssu/groupname/baseapplication/CameraActivity.java
@@ -0,0 +1,95 @@
+package ssu.groupname.baseapplication;
+
+
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class CameraActivity extends AppCompatActivity {
+
+    Camera camera;
+    FrameLayout cameraScreen;
+    ShowCamera showCamera;
+    String pathToFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        cameraScreen = (FrameLayout)findViewById(R.id.cameraScreen);
+        camera = Camera.open();
+        showCamera = new ShowCamera(this, camera);
+        cameraScreen.addView(showCamera);
+//
+//        Button btn = (Button)findViewById(R.id.captureButton);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                captureImage(v);
+//            }
+//        });
+
+        Button cBtn = (Button)findViewById(R.id.captureButton);
+        cBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage(v);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent cIntent = new Intent(CameraActivity.this, OperationsActivity.class);
+                startActivity(cIntent);
+            }
+        });
+    }
+    Camera.PictureCallback mPictureCallBack = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOutPutMediaFile();
+            if(picture_file == null) {
+                return;
+            } else {
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+                    camera.startPreview();
+                } catch(FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+    private File getOutPutMediaFile() {
+        String imageFileName = "temp.jpeg";
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)) {
+            return null;
+        } else {
+            File folder_TGS = new File(Environment.getExternalStorageDirectory() + File.separator + "TeamGoldSplit");
+            File outPutFile = new File(folder_TGS, imageFileName);
+            pathToFile = outPutFile.getAbsolutePath();
+            return outPutFile;
+        }
+    }
+    public void captureImage(View v) {
+        if(camera != null) {
+            camera.takePicture(null, null, mPictureCallBack);
+        }
+    }
+}
---
+++ app/src/main/java/ssu/groupname/baseapplication/FinalActivity.java
@@ -0,0 +1,66 @@
+package ssu.groupname.baseapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.strictmode.Violation;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FinalActivity extends FragmentActivity {
+
+    private Button topButton;
+    ColorPagerAdapter colorPagerAdapter;
+    private ViewPager cViewPager;
+    private ImageView imgView;
+    private ImageView imgView1;
+    private ImageView imgView2;
+
+    private ImageView[] views;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+
+        //topButton FIX THIS FIX THIS FIX THIS
+        topButton = findViewById(R.id.top_button);
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(FinalActivity.this, MainActivity.class);
+                backIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(backIntent);
+            }
+        });
+
+        String[] bmpFileNames = new String[6];
+        Bitmap bmp = null;
+        for(int i = 0; i < 6; i++){
+            String filename = getIntent().getStringExtra("color_image" + Integer.toString(i));
+            bmpFileNames[i] = filename;
+        }
+        colorPagerAdapter = new ColorPagerAdapter(getSupportFragmentManager());
+        cViewPager = findViewById(R.id.final_pager);
+        cViewPager.setAdapter(colorPagerAdapter);
+
+    }
+
+}
+
